7fae67765b05e9d3087949667a38c6aaa81974b2
rockey223 <rockeym50@gmail.com>
2026-03-15 11:37:10 +0545

    changes

--- src/utils/connectDb.js
+++ src/utils/connectDb.js
@@ -1,13 +1,22 @@
 import mongoose from "mongoose";
 
-const connectToDatabase = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    throw error;
-  }
-};
+const MONGODB_URI = process.env.MONGODB_URI;
 
-export default connectToDatabase;
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export default async function connectToDatabase() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
